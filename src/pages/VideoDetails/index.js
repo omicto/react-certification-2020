b5048d294a22ca929073065ad734d1a5e1150db1
omicto <tevas.dlr@gmail.com>
2021-03-30 15:12:03 -0700

    Layout video details page

--- src/pages/VideoDetails/index.js
+++ src/pages/VideoDetails/index.js
@@ -1,8 +1,14 @@
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router';
+import styled from 'styled-components';
 import Video from '../../components/Video/Video';
 import VideoSideBar from '../../components/Video/VideoSideBar/VideoSideBar.component';
 import { useVideoContext } from '../../providers/video';
+
+const Flex = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
 
 const VideoPage = () => {
   const { id } = useParams();
@@ -13,12 +19,14 @@ const VideoPage = () => {
   }, [id, searchRelatedVideos]);
 
   return (
-    <div>
+    <Flex>
       <div>
         <Video id={id} />
+        <p>Hola hola</p>
       </div>
+
       <VideoSideBar />
-    </div>
+    </Flex>
   );
 };
 
